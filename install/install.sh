c95006a3c6fa07aea33e022713afdff950292705
Matej Petrlik <petrlmat@fel.cvut.cz>
2020-02-07 16:53:07 +0100

    automated multicast enabling from readme

---
+++ install/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SYSCTL_FILE="/etc/sysctl.conf"
+MULTICAST_IP="224.0.0.1"
+
+#Install dependencies
+echo "Installing dependencies"
+sudo apt -y install ros-melodic-catch-ros libx264-dev libzstd-dev libqcustomplot-dev
+
+# Check whether multicast is enabled
+multicast_enabled=`cat /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts`
+if [[ "$multicast_enabled" == "1" ]]; then
+
+  echo "Enabling multicast temporarily"
+  sudo sh -c "echo 0 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts"
+
+  echo "Enabling multicast permanently"
+  sudo sh -c "echo net.ipv4.icmp_echo_ignore_broadcasts=0>>$SYSCTL_FILE"
+  
+  echo "Restarting procps service"
+  sudo service procps restart
+else 
+  echo "Multicast is already enabled"
+fi
+
+echo "Done"
+
