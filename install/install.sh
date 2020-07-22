1d56dbef59a6c12bb9d5b3474243930513171632
Vojtech Spurny <vojtech.spurny@fel.cvut.cz>
2020-07-22 15:57:26 +0200

    updated install script

--- install/install.sh
+++ install/install.sh
@@ -1,32 +1,84 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+unattended=0
+for param in "$@"
+do
+  echo $param
+  if [ $param="--unattended" ]; then
+    echo "installing in unattended mode"
+    unattended=1
+    subinstall_params="--unattended"
+  fi
+done
+
 SYSCTL_FILE="/etc/sysctl.conf"
+
+echo "$0: Installing nimbro_network package"
 
 # Install dependencies
 echo "Installing dependencies"
 sudo apt -y install ros-melodic-catch-ros libx264-dev libzstd-dev libqcustomplot-dev
 
-# Check whether multicast is enabled
-multicast_enabled=`cat /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts`
-if [[ "$multicast_enabled" == "1" ]]; then
+default=n
+while true; do
+  if [[ "$unattended" == "1" ]]
+  then
+    resp=y
+  else
+    [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mEnable multicast permanently? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+  fi
+  response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
-  echo "Enabling multicast temporarily"
-  sudo sh -c "echo 0 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts"
+  if [[ $response =~ ^(y|Y)=$ ]]
+  then
 
-  echo "Enabling multicast permanently"
-  sudo sh -c "echo net.ipv4.icmp_echo_ignore_broadcasts=0>>$SYSCTL_FILE"
-  
-  echo "Restarting procps service"
-  sudo service procps restart
-else 
-  echo "Multicast is already enabled"
-fi
+    # Check whether multicast is enabled
+    multicast_enabled=`cat /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts`
+    if [[ "$multicast_enabled" == "1" ]]; then
 
-# Set broadcast IP address
-~/git/uav_core/ros_packages/mrs_general/scripts/set_broadcast_rc_variable.sh
+      echo "Enabling multicast permanently"
+      sudo sh -c "echo net.ipv4.icmp_echo_ignore_broadcasts=0>>$SYSCTL_FILE"
+      
+      echo "Restarting procps service"
+      sudo service procps restart
+    else 
+      echo "Multicast is already enabled"
+    fi
 
-# Populate /etc/hosts
-~/git/uav_core/miscellaneous/dotssh/generate_ssh_config.sh
+    break
+  elif [[ $response =~ ^(n|N)=$ ]]
+  then
+    while true; do
+      [[ -t 0 ]] && { read -t 10 -n 2 -p $'\e[1;32mEnable multicast temporarily? [y/n] (default: '"$default"$')\e[0m\n' resp || resp=$default ; }
+      response=`echo $resp | sed -r 's/(.*)$/\1=/'`
 
-echo "Done"
+      if [[ $response =~ ^(y|Y)=$ ]]
+      then
+        echo "Enabling multicast temporarily"
+        sudo sh -c "echo 0 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts"
+
+        echo "Restarting procps service"
+        sudo service procps restart
+
+        break
+      elif [[ $response =~ ^(n|N)=$ ]]
+      then
+        break
+      else
+        echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+      fi
+    done
+    break
+  else
+    echo " What? \"$resp\" is not a correct answer. Try y+Enter."
+  fi
+
+done
+
+echo "$0: Done"
 
